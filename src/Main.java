f3e50a89f898d90808beb785d0419e15ae85f9f5
.. <semenokirik14@gmail.com>
2026-09-06 15:39:13 +0300

    Моя первая программа».

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
             System.out.println("Привет, " + name + "! Ты уже взрослый, садись за руль!");
         } else {
             int yearsLeft = 18 - age;
-            System.out.println("Привет, " + name + "! Через " + yearsLeft + " лет ты сможешь водить машину.");
+            System.out.println("Привет, " + name + "! Чериез " + yearsLeft + " лет ты сможешь водить машину.");
         }
 
         scanner.close();
